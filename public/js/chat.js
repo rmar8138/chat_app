4f9c3c2995d6f4d3966462cf6eb84f8090176538
rmar8138 <ragan.martinez@live.com>
2019-11-13 21:01:57 +1100

    Add share location feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector("#message-form").addEventListener("submit", e => {
 
   socket.emit("sendMessage", message);
 });
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  } else {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      socket.emit("sendLocation", {
+        latitude: coords.latitude,
+        longitude: coords.longitude
+      });
+    });
+  }
+});
